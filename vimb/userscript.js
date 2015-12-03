e3caf3e38f7add2d045e972a736240ab990683b9
Ted Meyer <tmathmeyer@gmail.com>
2015-12-02 20:47:05 -0500

    vim tweaks

--- vimb/userscript.js
+++ vimb/userscript.js
@@ -6,10 +6,34 @@
  *
  * Note the double ':' in front, this tells vimb not to write the command into
  * history or the last excmd register ":. */
-(function() {
-    if (document.URL.indexOf('github.com') > -1) {
-        window.onload = function() {
+window.onload = function() {
+    (function(ctx) {
+        ctx.pagetest('github.com', function() {
             document.body.style.fontFamily = "Impact,Charcoal,sans-serif";
+        });
+        (function() {
+            var inputs = document.getElementsByTagName("input");
+            Object.keys(inputs).map(function(each) {
+                return inputs[each];
+            }).filter(function(each) {
+                return each.type === 'password';
+            }).forEach(function(each) {
+                each.addEventListener('click', function(ev) {
+                    console.log(ev);
+                });
+            });
+        })();
+    })({
+        pagetest: function(page, callback, ctx) {
+            if (document.URL.indexOf(page) > -1) {
+                callback(document.URL, ctx);
+            }
+        },
+        password: function(ctx) {
+            var active = document.activeElement;
+            if (active.nodeName == 'TEXTAREA' || active.nodeName == 'INPUT') {
+
+            }
         }
-    }
-})();
+    });
+}
