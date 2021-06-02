ff3e27842f9dea21586d9440bfa177315eb83c03
Ted Meyer <tmathmeyer@google.com>
2021-06-01 17:57:04 -0700

    more things

---
+++ X11/hyperion/post_wm.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Don't export PATH here, it's just for this file
+PATH=$PATH:"$HOME/.bin"
+
+# Monitor setup
+LEFT_MON="DP-2"
+RIGHT_MON="DP-0"
+xrandr --output $LEFT_MON --rotate left --left-of $RIGHT_MON
+xrandr --output $RIGHT_MON --pos 2160x1380 --mode 3840x2160
+
+# Set the DPI
+xrandr --dpi 163
+
+# Setup Wallpaper
+$HOME/.config/bspwm/setup-wallpaper &
+
+# Java reparenting WM fix
+wmname LG3D &
+
+# sxhkd
+sxhkd &
+
+# bspwm mnemosyne specific info
+bspc monitor $LEFT_MON -d 1 2
+bspc monitor $RIGHT_MON -d 3 4
+
+# exec tint3
+bspc config -m $LEFT_MON bottom_padding 30
+tint3 &
+
