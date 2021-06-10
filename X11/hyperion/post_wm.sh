6c60f9d178b2b06a9e137ef8d9b4d113b42e11c1
Ted Meyer <tmathmeyer@google.com>
2021-06-09 19:07:10 -0700

    kvm

--- X11/hyperion/post_wm.sh
+++ X11/hyperion/post_wm.sh
@@ -4,8 +4,8 @@
 PATH=$PATH:"$HOME/.bin"
 
 # Monitor setup
-LEFT_MON="DP-2"
-RIGHT_MON="DP-0"
+LEFT_MON="HDMI-0"
+RIGHT_MON="HDMI-1"
 xrandr --output $LEFT_MON --rotate left --left-of $RIGHT_MON
 xrandr --output $RIGHT_MON --pos 2160x1380 --mode 3840x2160
 
--- X11/mnemosyne/monitor_setup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Monitor setup info for mnemosyne
-
-# DPI for this 4k thing is 331
-xrandr --dpi 331
